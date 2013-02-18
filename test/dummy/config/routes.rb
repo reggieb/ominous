37c40d3bbeaf5d3ae61a7d87b0a414184455f991
Rob Nichols <git@nicholshayes.co.uk>
2013-02-18 11:58:46 +0000

    Adds some helper tests

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   resources :things
+  
+  root :to => 'things#index'
 
   mount Ominous::Engine => "/ominous"
 end
--- test/unit/helpers/ominous/warnings_helper_test.rb
+++ test/unit/helpers/ominous/warnings_helper_test.rb
@@ -2,5 +2,23 @@ require 'test_helper'
 
 module Ominous
   class WarningsHelperTest < ActionView::TestCase
+    
+    def setup
+      @warning = Ominous::Warning.find(1)
+      @closer = @warning.closers.first
+    end
+
+    def test_classes_for_when_closer_hidden
+      @closer.update_attribute(:start_hidden, true)
+      expected = 'closer start_hidden'
+      assert_equal(expected, classes_for(@closer))
+    end
+
+    def test_classes_for_when_closer_visible
+      @closer.update_attribute(:start_hidden, false)
+      expected = 'closer start_visible'
+      assert_equal(expected, classes_for(@closer))
+    end  
+    
   end
 end
